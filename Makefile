e051f807ac68d8b5c81126bb4f51c615e7738053
Francisco Fernández-Victorio Hernández <franciscofvh@hotmail.com>
2024-05-13 21:47:15 +0200

    First code commit

---
+++ Makefile
@@ -0,0 +1,23 @@
+# since g++ does not compile sqlite3.c, make a module 'sqlite3.o'
+# and apply separate compilation, as shown for goal 'examplepp'
+
+sqlite3.o: sqlite3.c
+	gcc -c sqlite3.o sqlite3.c
+
+example: SQLite.ex00.cpp
+	gcc SQLite.ex00.c sqlite3.c
+
+*pp: *.cpp *.h SQLite.o
+	c++    -std=c++23 $< sqlite3.o -o $@
+
+examplepp: SQLite.ex00.cpp sqlite3.o
+	# g++ -std=c++11 -Wno-literal-suffix -o examplepp SQLite.ex00.cpp sqlite3.o
+	g++ -std=c++11                     -o examplepp SQLite.ex00.cpp sqlite3.o
+
+sqlite3: shell.c sqlite3.c sqlite3.h
+	gcc -DSQLITE_THREADSAFE=0 shell.c sqlite3.c -ldl -lm -o sqlite3
+
+clean:
+	$(RM) sqlite3 example examplepp a.out
+clean_all:
+	$(RM) sqlite3 example examplepp a.out *.o
---
+++ SQLite.ex00.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+// GPP means this file gets included, not separately commpiled
+#include "sqlite3.h"
+// #include "sqlite3.cpp"
+
+#include <iostream>
+
+/* the arguments in callback function 'callback' -- you may rename it to your liking--
+   are the number of columns, their name and their value.
+ * This function is supposed to be called on each row that is returned.
+ * Function callback was originally defined static, which I have commented out.
+ */
+/* static */ int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+  int i;
+  for(i=0; i<argc; i++)
+    std::cout << azColName[i] << (argv[i] ? argv[i] : "NULL");
+  std::cout << '\n';
+  return 0;
+}
+
+
+int main(int argc, char **argv){
+  sqlite3 *db;
+  char *zErrMsg = nullptr; // instead of " = 0;"
+  int rc; // usage: rc = sqlite3_function()
+
+  if( argc!=3 ) {
+    // fprintf(stderr, "Usage: %s DATABASE SQL-STATEMENT\n", argv[0]);
+    std::cerr << "Usage: " << argv[0] << " DATABASE SQL-STATEMENT\n";
+    return 1;
+  }
+
+  rc = sqlite3_open(argv[1], &db);
+  if( rc ){
+    // fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    std::cerr << "Can't open database: " << sqlite3_errmsg(db) << '\n';
+    sqlite3_close(db);
+    return 1;
+  }
+
+  rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
+  if( rc != SQLITE_OK ){
+    // fprintf(stderr, "SQL error: %s\n", zErrMsg);
+    std::cerr << "SQL error: " << zErrMsg << '\n';
+    sqlite3_free(zErrMsg);
+  }
+
+  sqlite3_close(db);
+  return 0;
+}
